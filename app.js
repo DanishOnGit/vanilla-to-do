849c8928546f31a0ea82b95bacd0040156de6370
DanishOnGit <youcandoit1911@gmail.com>
2021-01-22 15:45:54 +0530

    style: colors and padding of items

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let addBtn=document.querySelector(".toDoBtn")
 let todolist=document.querySelector(".toDoList")
 
 addBtn.addEventListener("click",addItems)
+todolist.addEventListener("click",deletecheck)
 
 
 
@@ -14,17 +15,25 @@ function addItems(e){
     toDoDiv.classList.add("toDo");
     const newToDo=document.createElement("li");
     newToDo.classList.add("toDo-item");
-    newToDo.innerText="hi there"
+    newToDo.innerText=input.value
 
     //append list item to div
     toDoDiv.appendChild(newToDo);
+
     const completedBtn=document.createElement("button");
     completedBtn.innerHTML='<i class="fas fa-check"></i>';
     completedBtn.classList.add("completed-Btn");
     toDoDiv.appendChild(completedBtn)
 
     const deleteBtn=document.createElement("button");
-    completedBtn.innerHTML='<i class="fas fa-trash"></i>';
-    completedBtn.classList.add("delete-Btn");
+    deleteBtn.innerHTML='<i class="fas fa-trash"></i>';
+    deleteBtn.classList.add("delete-Btn");
     toDoDiv.appendChild(deleteBtn);
+
+    todolist.appendChild(toDoDiv)
+    //clear input field
+    input.value=""
+}
+function deletecheck(e){
+    console.log(e.target)
 }
